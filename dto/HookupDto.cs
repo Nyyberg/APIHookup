ed1da985c2856675283951e4b7b8f04ed28b01c6
kasp441 <91524039+kasp441@users.noreply.github.com>
2023-12-18 14:51:07 +0100

    quick refactor of hookupDto

--- dto/HookupDto.cs
+++ dto/HookupDto.cs
@@ -7,16 +7,18 @@ namespace apihookup.dto
     /// </summary>
     public class HookupDto
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
 
         public string? Url { get; set; }
 
+        public string? MethodType { get; set; }
+
         public bool? IntervalIsEveryDay { get; set; }
 
+        public bool? IntervalIsEveryWeek { get; set; }
+
+        public bool? IntervalIsEveryMonth { get; set; }
+
         public DateTime? IntervalTimeOfDay { get; set; }
-
-        public int? CustomCalendarBeId { get; set; }
-
-        public string? MethodType { get; set; }
     }
 }
